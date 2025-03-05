8149c08b315b5dd775f10822884ccb1d85ac0284
Ravishankar Soni <soniravi9826@gmail.com>
2025-03-05 13:40:00 +0530

    Add files via upload

---
+++ Assets/js/lynx.js
@@ -0,0 +1,12 @@
+// Login button functionality
+const loginBtn = document.getElementById('login-btn');
+loginBtn.addEventListener('click', () => {
+    alert('Login functionality will be implemented here!');
+});
+
+// Contact button functionality
+const contactBtn = document.getElementById('contact-btn');
+contactBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    alert('Contact form will be implemented here!');
+});
